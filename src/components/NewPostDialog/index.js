4e099e61c512c971429799f6db3f2b34f3f92dd4
Brandon Day <brandontday7@gmail.com>
2018-10-08 14:53:39 -0400

    started making a modal for adding a new track

--- src/components/NewPostDialog/index.js
+++ src/components/NewPostDialog/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
 import PostForm from "./PostForm"
 	
 class NewPostDialog extends Component {
@@ -20,29 +19,30 @@ class NewPostDialog extends Component {
   	const { open, closeDialog } = this.props
   	const { title, description, artistName, trackTitle, image } = this.state
   	return (
-		  <Dialog
-		    actions={
-		      <div>
-		        <button
-		          type="button"
-		          className="alert-button"
-		          onClick={closeDialog}
-		          >
-		          CANCEL
-		        </button>
-		      </div>
-		    }
-		    open={open}
-		    >
-		    <PostForm 
-		    	title={title}
-		    	description={description}
-		    	artistName={artistName}
-		    	trackTitle={trackTitle}
-		    	image={image}
-		    	onChange={this.onChange}
-		    />
-		  </Dialog>
+  		<div>
+  		{ open ? 
+	  		<div className="modal-backing">
+	  			<div className="modal">
+		 		    <PostForm 
+				    	title={title}
+				    	description={description}
+				    	artistName={artistName}
+				    	trackTitle={trackTitle}
+				    	image={image}
+				    	onChange={this.onChange}
+				    />
+				    <div className="buttons-container">
+				    	<div className="create button">
+				    		Create
+				    	</div>
+					    <div className="cancel button">
+					    	Cancel
+					    </div>
+				    </div>
+	  			</div>
+	  		</div> : null
+  		}
+  		</div>
   	)
 	}
 }
